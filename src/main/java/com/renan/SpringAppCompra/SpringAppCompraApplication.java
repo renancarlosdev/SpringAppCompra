671250c67281b2dd012dae4f05e1547f74a7bb66
renancarlosdev <renancarlosgr@hotmail.com>
2022-08-11 15:44:48 -0300

    Projeto criado

---
+++ src/main/java/com/renan/SpringAppCompra/SpringAppCompraApplication.java
@@ -0,0 +1,13 @@
+package com.renan.SpringAppCompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppCompraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renan/SpringAppCompra/SpringAppCompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.SpringAppCompra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppCompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
